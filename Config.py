d25eb4a35a0876db8eeb7e4fb2dc22666da89e7e
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-03-21 18:54:09 +0530

    Aᴜᴛᴏ-ғɪxᴇᴅ !

---
+++ Config.py
@@ -0,0 +1,6 @@
+from os import environ as env
+
+API_ID = int(env.get("API_ID", "26850449"))
+API_HASH = str(env.get("API_HASH", "72a730c380e68095a8549ad7341b0608"))
+BOT_TOKEN = str(env.get("BOT_TOKEN", "6486942911:AAFsg5NLnpHeosWQNcFz2NtvhtUF1y311Sw"))
+SUDOERS = list(map(int, env.get("SUDOERS", "6517565595 5896960462 5220416927").split()))
