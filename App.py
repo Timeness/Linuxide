81c6c1433e5aabe9e483baa653b338ec15495582
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-03-21 16:45:42 +0530

    Update App.py

--- App.py
+++ App.py
@@ -54,7 +54,7 @@ class Pyro():
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
         bot_token=Config.BOT_TOKEN,
-        plugins=dict(root="Packages")
+        plugins=dict(root="Packages.")
     )
 app = Pyro.Soumo
 
