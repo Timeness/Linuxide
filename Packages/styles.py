5accb65d8b28857a20e6769c200ea6d3f2c6c547
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-03-21 15:44:46 +0530

    Update styles.py

--- Packages/styles.py
+++ Packages/styles.py
@@ -1,5 +1,5 @@
 from App import app, mongo, add_user
 
 
-@app.on_message(filters.command("style", ["?", "/", ".", "!", "$"]))
-async def styles_Funcs()
+#@app.on_message(filters.command("style", ["?", "/", ".", "!", "$"]))
+
