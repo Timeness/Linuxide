0df94253bc41f3c5872478716fd4e351f4bc15ca
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-03-21 16:43:17 +0530

    Update __init__.py

--- Linux/__init__.py
+++ Linux/__init__.py
@@ -1,1 +1,25 @@
 
+    
+class Pyro():
+    Soumo = PyGram(
+        name="PyroSoumo",
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        bot_token=Config.BOT_TOKEN,
+        plugins=dict(root="Packages"),
+        workers=Config.SELF_WORKERS,
+        in_memory=True
+    )
+App = Pyro.Soumo
+
+class Hydro():
+    Soumo = HyGram(
+        name="HydroSoumo",
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        bot_token=Config.BOT_TOKEN,
+        plugins=dict(root="Packages"),
+        workers=Config.SELF_WORKERS,
+        in_memory=True
+    )
+Sakura = Hydro.Soumo
