ca22b24c234b410bd4527fc015b91d9c5a5fd6c4
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-03-22 17:20:44 +0530

    //

--- Packages/lockWelPrem.py
+++ Packages/lockWelPrem.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message
 @client.on_message(filters.new_chat_members)
 async def member_premium(client: client, message: Message) -> None:
     check = message.new_chat_members[0]
-    user = await client.get_chat(check.id)
+    user = await client.get_user(check.id)
     if await wepre.find_one({"chat_id": message.chat.id}):
         if user.is_premium:
             await client.ban_chat_member(message.chat.id, user.id)
@@ -21,13 +21,15 @@ wepre = db["Soumo"]["PremWe"]
 
 @client.on_message(filters.command("welcomelock"))
 async def welcomeLock(client: client, message: Message) -> None:
-    if len(message.command) > 1:
+    if len(message.command) == 1:
         return await message.reply("**• Usᴀɢᴇ sʏɴᴛᴀx: /welcomelock [ᴍᴏᴅᴇs]**")
-    modes = message.text.split(None, 1)[1].strip()
+    modes = message.text.split(None, 1)[1]
     if modes == "premium":
         if await wepre.find_one({"chat_id": message.chat.id}):
             await message.reply("**ᴡᴇʟᴄᴏᴍᴇ ʟᴏᴄᴋ ᴘʀᴇᴍɪᴜᴍ ᴀʟʀᴇᴀᴅʏ ᴇɴᴀʙʟᴇᴅ ʜᴇʀᴇ.**")
         else:
             await wepre.insert_one({"chat_id": message.chat.id})
             await message.reply("**ʟᴏᴄᴋᴇᴅ ᴡᴇʟᴄᴏᴍᴇ ᴘʀᴇᴍɪᴜᴍ ᴜsᴇʀ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ.**")
+    else:
+        await message.reply("**ᴄʜᴇᴄᴋ ᴡᴇʟᴄᴏᴍᴇ ʟᴏᴄᴋ ᴍᴏᴅᴇs.**")
   
