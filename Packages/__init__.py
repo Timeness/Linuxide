b0cdc353deb9f2083160f0953ee2c260cd4fd54d
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-03-23 16:50:11 +0530

    Aᴜᴛᴏ-ғɪxᴇᴅ !

--- Packages/__init__.py
+++ Packages/__init__.py
@@ -1,14 +1,1 @@
-import glob
-from os.path import basename, dirname, isfile
 
-def __acc_modes():
-    mod_pass = glob.glob(dirname(__file__) + "/*.py")
-    aoc_modes = [
-        basename(so)[:-3]
-        for so in mod_pass
-        if isfile(so) and so.endswith(".py") and not so.endswith("__init__.py") and not so.endswith("cloneFunc.py") and not so.endswith("syncFunc.py")
-    ]
-    return aoc_modes
-
-ALL_MODULES = sorted(__acc_modes())
-__all__ = ALL_MODULES + ["ALL_MODULES"]
