3547bc983fe71897eb111f1ed3db5007c9c3446c
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-03-23 16:19:35 +0530

    Aᴜᴛᴏ-ғɪxᴇᴅ !

--- Linux/__init__.py
+++ Linux/__init__.py
@@ -1,6 +1,7 @@
 import sys
 import Config
 import logging, asyncio, time
+from Graph import Clean_Stage
 from telegram.ext import Application
 from pyrogram import Client as PyGram
 from hydrogram import Client as HyGram
@@ -26,6 +27,7 @@ logging.basicConfig(
         logging.StreamHandler(),
     ]
 )
+logging.getLogger('httpx').setLevel(logging.WARNING)
 logging.getLogger("asyncio").setLevel(logging.CRITICAL)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logging.getLogger("hydrogram").setLevel(logging.WARNING)
@@ -34,9 +36,11 @@ LOGGER = logging.getLogger(__name__)
 if not Config.API_ID:
     LOGGER.warning("» Wᴀʀɴɪɴɢ: ᴀᴘɪ_ɪᴅ ɴᴏᴛ ғᴏᴜɴᴅ ɪɴ ᴄᴏɴғɪɢ ғɪʟᴇs sʜᴜᴛᴅᴏᴡɴ ʙᴏᴛ !")
     sys.exit()
+    
 elif not Config.API_HASH:
     LOGGER.warning("» Wᴀʀɴɪɴɢ: ᴀᴘɪ_ʜᴀsʜ ɴᴏᴛ ғᴏᴜɴᴅ ɪɴ ᴄᴏɴғɪɢ ғɪʟᴇs sʜᴜᴛᴅᴏᴡɴ ʙᴏᴛ !")
     sys.exit()
+    
 elif not Config.BOT_TOKEN:
    LOGGER.warning("» Wᴀʀɴɪɴɢ: ʙᴏᴛ_ᴛᴏᴋᴇɴ ɴᴏᴛ ғᴏᴜɴᴅ ɪɴ ᴄᴏɴғɪɢ ғɪʟᴇs sʜᴜᴛᴅᴏᴡɴ ʙᴏᴛ !")
    sys.exit()
@@ -63,8 +67,29 @@ class Hydro():
     )
 Sakura = Hydro.Soumo
 
+async def addPackages():
+    await App.start()
+    await Sakura.start()
+    try:
+        Resocs = await Clean_Stage()
+        if Resocs:
+            await App.edit_message_text(
+                Resocs["chat_id"],
+                Resocs["message_id"],
+                "**🔮 Lɪɴᴜxɪᴅᴇ Rᴇsᴛᴀʀᴛᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ !**"
+            )
+        else:
+            await App.send_message(Config.SUPPORT, "**🔮 Lɪɴᴜxɪᴅᴇ Cʟᴏᴜᴅ Sᴇʀᴠᴇʀ Sᴛᴀʀᴛᴇᴅ !**")
+    except Exception:
+        LOGGER.info("» Bᴏᴛ ʜᴀs ғᴀɪʟᴇᴅ ᴛᴏ ᴀᴄᴄᴇss ᴛʜᴇ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ. ᴍᴀᴋᴇ sᴜʀᴇ ᴛʜᴀᴛ ʏᴏᴜ ʜᴀᴠᴇ ᴀᴅᴅᴇᴅ ʏᴏᴜʀ ʙᴏᴛ ᴛᴏ ʏᴏᴜʀ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ.")
+
 pyApp = Application.builder().token(Config.BOT_TOKEN).build()
 Func = pyApp.add_handler
 asyncio.get_event_loop().run_until_complete(
-    asyncio.gather(pyApp.bot.initialize())
+    asyncio.gather(pyApp.bot.initialize(), addPackages())
 )
+
+LOGGER.info("[INFO]: Gᴇᴛᴛɪɴɢ Bᴏᴛ Iɴғᴏʀᴍᴀᴛɪᴏɴs...")
+BOT_ID = pyApp.bot.id
+BOT_NAME = pyApp.bot.first_name
+BOT_USERNAME = pyApp.bot.username
