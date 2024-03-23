8235554470bef03e94634e71540428ed2e3d1f42
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-03-23 21:08:22 +0530

    //

--- Linux/__main__.py
+++ Linux/__main__.py
@@ -3,7 +3,7 @@ import asyncio
 import traceback
 from telegram import Update
 from importlib import import_module
-from Linux import Loop, pyApp, LOGGER
+from Linux import Loop, pyApp
 from Linux.Modules import ALL_MODULES
 from telegram.ext import ContextTypes
 from telegram.error import (
@@ -13,53 +13,54 @@ from telegram.error import (
 # Iᴍᴘᴏᴀʀᴛᴀʙʟᴇ ᴍᴏᴅᴜʟᴇs ғᴜɴᴄᴛɪᴏɴ...
 for Modes in ALL_MODULES:
     import_module("Linux.Modules." + Modes)
-LOGGER.info("Sᴜᴄᴄᴇssғᴜʟʟʏ Lᴏᴀᴅᴇᴅ Mᴏᴅᴜʟᴇs :" + str(Modes))
+#LOGGER.info("Sᴜᴄᴄᴇssғᴜʟʟʏ Lᴏᴀᴅᴇᴅ Mᴏᴅᴜʟᴇs :" + str(Modes))
 
 async def error_SysFunc(update: Update, context: ContextTypes.DEFAULT_TYPE):
     error = context.error
     try:
         raise error
     except Forbidden:
-        LOGGER.info("» Nᴏɴᴇ ! Cᴜʀʀᴇɴᴛ Tᴇsᴛ Oɴɢᴏɪɴɢ ᴠ1.")
-        LOGGER.info(error)
+        #LOGGER.info("» Nᴏɴᴇ ! Cᴜʀʀᴇɴᴛ Tᴇsᴛ Oɴɢᴏɪɴɢ ᴠ1.")
+        #LOGGER.info(error)
         # ʀᴇᴍᴏᴠᴇ ᴜᴘᴅᴀᴛᴇ.ᴄʜᴀᴛ_ɪᴅ ғʀᴏᴍ ᴄᴏɴᴠᴇʀsᴀᴛɪᴏɴ ʟɪsᴛ
     except BadRequest:
-        LOGGER.info("» Nᴏɴᴇ ! Cᴜʀʀᴇɴᴛ Tᴇsᴛ Oɴɢᴏɪɴɢ ᴠ2.")
-        LOGGER.info("» Bᴀᴅʀᴇǫᴜᴇsᴛ Cᴀᴜɢʜᴛ !")
-        LOGGER.info(error)
+        #LOGGER.info("» Nᴏɴᴇ ! Cᴜʀʀᴇɴᴛ Tᴇsᴛ Oɴɢᴏɪɴɢ ᴠ2.")
+        #LOGGER.info("» Bᴀᴅʀᴇǫᴜᴇsᴛ Cᴀᴜɢʜᴛ !")
+        #LOGGER.info(error)
         # ʜᴀɴᴅʟᴇ ᴍᴀʟғᴏʀᴍᴇᴅ ʀᴇǫᴜᴇsᴛs - ʀᴇᴀᴅ ᴍᴏʀᴇ ʙᴇʟᴏᴡ !
     except TimedOut:
-        LOGGER.info("» Nᴏɴᴇ ! Cᴜʀʀᴇɴᴛ Tᴇsᴛ Oɴɢᴏɪɴɢ ᴠ3.")
+        #LOGGER.info("» Nᴏɴᴇ ! Cᴜʀʀᴇɴᴛ Tᴇsᴛ Oɴɢᴏɪɴɢ ᴠ3.")
         # ʜᴀɴᴅʟᴇ sʟᴏᴡ ᴄᴏɴɴᴇᴄᴛɪᴏɴ ᴘʀᴏʙʟᴇᴍs
     except NetworkError:
-        LOGGER.info("» Nᴏɴᴇ ! Cᴜʀʀᴇɴᴛ Tᴇsᴛ Oɴɢᴏɪɴɢ ᴠ4.")
+        #LOGGER.info("» Nᴏɴᴇ ! Cᴜʀʀᴇɴᴛ Tᴇsᴛ Oɴɢᴏɪɴɢ ᴠ4.")
         # ʜᴀɴᴅʟᴇ ᴏᴛʜᴇʀ ᴄᴏɴɴᴇᴄᴛɪᴏɴ ᴘʀᴏʙʟᴇᴍs
     except ChatMigrated as eorr:
-        LOGGER.info("» Nᴏɴᴇ ! Cᴜʀʀᴇɴᴛ Tᴇsᴛ Oɴɢᴏɪɴɢ ᴠ5.")
-        LOGGER.info(eorr)
+        #LOGGER.info("» Nᴏɴᴇ ! Cᴜʀʀᴇɴᴛ Tᴇsᴛ Oɴɢᴏɪɴɢ ᴠ5.")
+        #LOGGER.info(eorr)
         # ᴛʜᴇ ᴄʜᴀᴛ_ɪᴅ ᴏғ ᴀ ɢʀᴏᴜᴘ ʜᴀs ᴄʜᴀɴɢᴇᴅ ᴜsᴇ ᴇ.ɴᴇᴡ_ᴄʜᴀᴛ_ɪᴅ ɪɴsᴛᴇᴀᴅ
     except TelegramError:
-        LOGGER.info(error)
+        #LOGGER.info(error)
         # ʜᴀɴᴅʟᴇ ᴀʟʟ ᴏᴛʜᴇʀ ᴛᴇʟᴇɢʀᴀᴍ ʀᴇʟᴀᴛᴇᴅ ᴇʀʀᴏʀs
 
 def secPy_Run() -> None:
-    LOGGER.info("» Pʏᴛʜᴏɴ ᴛᴇʟᴇɢʀᴀᴍ ʙᴏᴛ ᴄʟɪᴇɴᴛ ʟᴀʏᴇʀ sᴛᴀʀᴛᴇᴅ !")
+    #LOGGER.info("» Pʏᴛʜᴏɴ ᴛᴇʟᴇɢʀᴀᴍ ʙᴏᴛ ᴄʟɪᴇɴᴛ ʟᴀʏᴇʀ sᴛᴀʀᴛᴇᴅ !")
     pyApp.add_error_handler(error_SysFunc)
     pyApp.run_polling(timeout=15, drop_pending_updates=True, allowed_updates=Update.MESSAGE)
 
 if __name__ == "__main__":
     try:
         secPy_Run()
-        LOGGER.info("Lɪɴᴜxɪᴅᴇ Sᴛᴀʀᴛᴇᴅ As Pʏᴛʜᴏɴ Tᴇʟᴇɢʀᴀᴍ Bᴏᴛ Vᴇʀsɪᴏɴ !")
+        #LOGGER.info("Lɪɴᴜxɪᴅᴇ Sᴛᴀʀᴛᴇᴅ As Pʏᴛʜᴏɴ Tᴇʟᴇɢʀᴀᴍ Bᴏᴛ Vᴇʀsɪᴏɴ !")
     except KeyboardInterrupt:
         pass
     except Exception:
         eocs = traceback.format_exc()
-        LOGGER.info(f"Eʀʀᴏʀ Oᴄᴄᴜʀʀᴇᴅ : {eocs}")
+        print(eocs)
+        #LOGGER.info(f"Eʀʀᴏʀ Oᴄᴄᴜʀʀᴇᴅ : {eocs}")
     finally:
         try:
             if Loop.is_running():
                 Loop.stop()
         finally:
             Loop.close()
-        LOGGER.info("------------------------ Lɪɴᴜxɪᴅᴇ Sᴛᴏᴘᴘᴇᴅ Sᴇʀᴠɪᴄᴇs ------------------------")
+        #LOGGER.info("------------------------ Lɪɴᴜxɪᴅᴇ Sᴛᴏᴘᴘᴇᴅ Sᴇʀᴠɪᴄᴇs ------------------------")
