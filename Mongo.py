e0957f66a457510d184f204fbc69d737538b485a
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-04-07 20:02:57 +0530

    // Success

---
+++ Mongo.py
@@ -0,0 +1,39 @@
+import Config
+from motor.motor_asyncio import AsyncIOMotorClient
+
+Mongo = AsyncIOMotorClient(Config.MONGO)
+user = Mongo["Gram"]["user"]
+
+async def add_user(user_id: int, user_name: str):
+    checkdb = await check_user(user_id)
+    if checkdb:
+        return
+    return await user.insert_one(
+        {
+            "user_id": user_id,
+            "user_name": user_name,
+            "coins": 2500,
+            "gems": 100,
+            "manna": 15,
+            "referral": 0,
+            "referral_reward": 0,
+            "invited": None
+        }
+    )
+
+async def check_user(user_id: int):
+    users = await user.find_one({"user_id" : user_id})
+    if not users:
+        return False
+    return True
+
+async def remove_user(user_id: int):
+    users = await user.find_one({"user_id": user_id})
+    if users:
+        return await user.delete_one({"user_id": user_id})
+
+async def user_data(user_id: int):
+    user = await user.find_one({"user_id" : user_id})
+    if not user:
+        return
+    return user
