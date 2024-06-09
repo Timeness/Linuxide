852504a854a8272a6707bca04804a9fded81496a
Sᴏυʀαʏєɴ <157188588+Sourayen@users.noreply.github.com>
2024-06-09 11:28:34 +0530

    done

--- Config.py
+++ Config.py
@@ -6,7 +6,7 @@ SUPPORT = int(env.get("SUPPORT", "-1001535967539"))
 PREFIXS = list(env.get("PREFIXS", "? * $ . ! /").split())
 API_HASH = str(env.get("API_HASH", "72a730c380e68095a8549ad7341b0608"))
 BOT_TOKEN = str(env.get("BOT_TOKEN", "6486942911:AAFsg5NLnpHeosWQNcFz2NtvhtUF1y311Sw"))
-SUDOERS = list(map(int, env.get("SUDOERS", "6517565595 5896960462 5220416927").split()))
+SUDOERS = list(map(int, env.get("SUDOERS", "6505111743 6517565595 5896960462 5220416927").split()))
 GIST_IMAGE = str(env.get("GIST_IMAGE", "https://graph.org/file/5cb4b1a7faec81476f99c.jpg"))
 GUARDS = list(map(int, env.get("GUARDS", " ").split()))
 
